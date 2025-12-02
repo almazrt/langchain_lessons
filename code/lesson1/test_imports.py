7e7bae904ba4e319b3fa8cd8fae216707386ef9c
Almaz <almazrt@gmail.com>
2025-12-02 12:29:41 +0300

    v2

--- code/lesson1/test_imports.py
+++
@@ -1,8 +0,0 @@
-# Тестовый файл для проверки импортов
-import os
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnableSequence
-
-print("Все модули успешно импортированы!")
--- code/lesson4/test_imports.py
+++
@@ -1,9 +0,0 @@
-# Тестовый файл для проверки импортов
-import os
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-from langchain_classic.agents import create_react_agent, AgentExecutor
-from langchain_core.prompts import PromptTemplate
-from langchain_core.tools import tool
-
-print("Все модули успешно импортированы!")
